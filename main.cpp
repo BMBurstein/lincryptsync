a21cb17dbe764bf1824fe5d752cb981b5cf991d5
Baruch Burstein <bmburstein@gmail.com>
2018-07-12 08:01:43 +0300

    Add message to indicate running

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 
 #include <thread>
 #include <chrono>
+#include <iostream>
 using namespace std::chrono_literals;
 
 int main() {
     DirPair pair{"/home/baruch/test/clrsync", "/home/baruch/test/cryptsync", EncType::z7};
     pair.sync();
 
+    std::cout << "Syncing...\n";
     for(int i=0; i<60; ++i) {
         std::this_thread::sleep_for(2s);
         pair.sync();
